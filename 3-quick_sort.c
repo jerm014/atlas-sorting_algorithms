a48b9182f27a3949b92d04577ae5bbd8968ae13a
jerm014 <jeremy.mitts@atlasschool.com>
2024-04-08 23:16:41 +0000

    general clean up

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,78 +1,6 @@
 #include "sort.h"
 
 /**
-* partition- partition the array and pick the last element as pivot
-*            (lomuto partition scheme)
-*
-* @arr:      the array
-* @low:      lowest point
-* @high:     highest point
-* @size:     array size
-*
-* Return:    index of the pivot
-*
-*/
-int partition(int *arr, int low, int high, size_t size)
-{
-	int pivot = arr[high];
-
-	int i = low - 1, j;
-
-	for (j = low; j <= high; j++)
-	{
-		if (arr[j] < pivot && arr[++i] != arr[j])
-		{
-			quick_sort_sort_swap(&arr[i], &arr[j]);
-			print_array(arr, size);
-		}
-	}
-	if (arr[++i] != arr[high])
-	{
-		quick_sort_sort_swap(&arr[i], &arr[high]);
-		print_array(arr, size);
-	}
-	return (i);
-}
-
-/**
-* quick_recursion- recursively sort partition until array is sorted
-* @arr:            array
-* @low:            lowest point
-* @high:           highest point
-* @size:           array size
-*
-*/
-void quick_recursion(int *arr, int low, int high, size_t size)
-{
-	int pivot_index;
-
-	if (low < high)
-	{
-		pivot_index = partition(arr, low, high, size);
-
-		quick_recursion(arr, low, pivot_index - 1, size);
-		quick_recursion(arr, pivot_index + 1, high, size);
-	}
-}
-
-/**
- * quick_sort_sort_swap- swap two integers
- *
- * @a:                   pointer to int
- * @b:                   pointer to int
- *
- * Return:               nothing
- */
-void quick_sort_sort_swap(int *a, int *b)
-{
-	int swap;
-
-	swap = *a;
-	*a = *b;
-	*b = swap;
-}
-
-/**
  * quick_sort- quicksort an array of integers
  *
  * @array:     array to be sorted
@@ -82,5 +10,80 @@ void quick_sort_sort_swap(int *a, int *b)
  */
 void quick_sort(int *array, size_t size)
 {
-	quick_recursion(array, 0, (int)(size - 1), size);
+	quick_sort_recursion(array, 0, (int)(size - 1), size);
+}
+
+/**
+* quick_sort_recursion- recursively sort partition until array is sorted
+*
+* @arr:                 array
+* @low:                 lowest point
+* @high:                highest point
+* @size:                array size
+*
+* Return:               nothing
+*
+*/
+void quick_sort_recursion(int *arr, int low, int high, size_t size)
+{
+	int pivot_index;
+
+	if (low < high)
+	{
+		pivot_index = quick_sort_partition(arr, low, high, size);
+
+		quick_sort_recursion(arr, low, pivot_index - 1, size);
+		quick_sort_recursion(arr, pivot_index + 1, high, size);
+	}
+}
+
+/**
+* quick_sort_partition- partition the array and pick the last element as pivot
+*                       (lomuto partition scheme)
+*
+* @arr:                 the array
+* @low:                 lowest point
+* @high:                highest point
+* @size:                array size
+*
+* Return:               index of the pivot
+*
+*/
+int quick_sort_partition(int *arr, int low, int high, size_t size)
+{
+	int pivot = arr[high];
+
+	int i = low - 1, j;
+
+	for (j = low; j <= high; j++)
+	{
+		if (arr[j] < pivot && arr[++i] != arr[j])
+		{
+			quick_sort_swap(&arr[i], &arr[j]);
+			print_array(arr, size);
+		}
+	}
+	if (arr[++i] != arr[high])
+	{
+		quick_sort_swap(&arr[i], &arr[high]);
+		print_array(arr, size);
+	}
+	return (i);
+}
+
+/**
+ * quick_sort_swap- swap two integers
+ *
+ * @a:                   pointer to int
+ * @b:                   pointer to int
+ *
+ * Return:               nothing
+ */
+void quick_sort_swap(int *a, int *b)
+{
+	int swap;
+
+	swap = *a;
+	*a = *b;
+	*b = swap;
 }
--- sort.h
+++ sort.h
@@ -18,15 +18,21 @@ typedef struct listint_s
 } listint_t;
 
 /* Functions */
+
+/* 0 */
 void bubble_sort(int *, size_t);
 void bubble_sort_swap(int *, int *);
+/* 1 */
 void insertion_sort_list(listint_t **);
 void insertion_sort_list_swap(listint_t **, listint_t **, listint_t *);
+/* 2 */
 void selection_sort(int *, size_t);
 void selection_sort_swap(int *, int *);
+/* 3 */
 void quick_sort(int *array, size_t size);
-void quick_sort_sort(int *, size_t, size_t, size_t);
-void quick_sort_sort_swap(int *a, int *b);
+int quick_sort_partition(int *, int, int, size_t);
+void quick_sort_recursion(int *, int, int, size_t);
+void quick_sort_swap(int *, int *);
 
 /* Support Functions */
 void print_array(const int *array, size_t size);
